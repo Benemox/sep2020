fd80b50bf27645cb701c30e5746472704c0d1500
Benemox <abelyaesta@hotmail.com>
2020-09-22 12:48:37 +0200

    pendiente22

---
+++ file.js
@@ -0,0 +1,28 @@
+//¡/* mirar  input: checked + div {display:}
+// div {display: none}
+//div {display block}
+//html ;
+// label for= cd 
+// input id = cd type checkbox
+// div ----h1--p
+/*
+animacion en cuadrado;
+div {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+  position: relative;
+  animation: example 4s infinite;
+}
+
+@keyframes example {
+  0%   {background-color:red; left:0px; top:0px;}
+  25%  {background-color:yellow; left:200px; top:0px;}
+  50%  {background-color:blue; left:200px; top:200px;}
+  75%  {background-color:green; left:0px; top:200px;}
+  100% {background-color:red; left:0px; top:0px;}
+}
+
+
+
+*/
